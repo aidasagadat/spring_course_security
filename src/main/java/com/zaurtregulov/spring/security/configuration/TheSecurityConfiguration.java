f33e8252524c63b946c8f3248280d1754b567f86
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-27 19:17:35 +0500

    Configure Spring Security with JDBC authentication and role-based authorization

--- src/main/java/com/zaurtregulov/spring/security/configuration/TheSecurityConfiguration.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/TheSecurityConfiguration.java
@@ -1,23 +1,34 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class TheSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("aida").password("aida").roles("CEO"))
-                .withUser(userBuilder.username("dilnaz").password("dilnaz").roles("HR"))
-                .withUser(userBuilder.username("adiya").password("adiya").roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
 
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("CEO", "HR", "MANAGER")
+                .antMatchers("/ceo_info").hasRole("CEO")
+                .antMatchers("/manager_info").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
