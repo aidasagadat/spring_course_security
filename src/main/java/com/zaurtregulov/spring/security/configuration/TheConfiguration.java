ceeb5aa1f2356f819658d83a3aed33a4fa47acba
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-27 19:16:48 +0500

    Configure C3P0 DataSource bean for MySQL connection

--- src/main/java/com/zaurtregulov/spring/security/configuration/TheConfiguration.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/TheConfiguration.java
@@ -1,12 +1,16 @@
 package com.zaurtregulov.spring.security.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.zaurtregulov.spring.security")
@@ -19,4 +23,45 @@ public class TheConfiguration {
         return internalResourceViewResolver;
     }
 
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try{
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        return dataSource;
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
