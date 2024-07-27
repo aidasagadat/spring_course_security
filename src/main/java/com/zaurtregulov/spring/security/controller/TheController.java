195c8e9f9e16b01f2f92f9faa2805b203ca9c83e
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-27 19:15:57 +0500

    Add endpoints for viewing all employees, CEO information, and manager information

--- src/main/java/com/zaurtregulov/spring/security/controller/TheController.java
+++ src/main/java/com/zaurtregulov/spring/security/controller/TheController.java
@@ -1,7 +1,27 @@
 package com.zaurtregulov.spring.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TheController {
+
+    @GetMapping("/")
+    public String getAllEmployees(){
+        return "view_all_employees";
+    }
+
+
+    @GetMapping("/ceo_info")
+    public String showCEOInfo(){
+        return "view_for_ceo";
+    }
+
+    @GetMapping("/manager_info")
+    public String showManagerInfo(){
+        return "view_for_managers";
+    }
+
+
+
 }
